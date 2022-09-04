87fc16292825a2bc6e1ec91064b557cbe7f799d2
dxq <tonyriddick1527@gmail.com>
2022-09-04 19:47:40 +0800

    1.add PDRead to SharedMethod 2.change some logic in Log.py 3.create python format to this project

--- Log.py
+++ Log.py
@@ -14,67 +14,142 @@ try:
 except:
     raise ModuleNotFoundError("module loguru is needed!")
 
-def MKOutput():
-    if not os.path.exists("./output"):
-        os.makedirs("output")
+_log_full_print = False
+
+
+def SplitDirFile(s):
+    dir_end = s.rfind("/")
+    if dir_end != -1:
+        return s[:dir_end], s[dir_end + 1 :]
+    else:
+        return None, s
+
+
+def MKDirsToFile(s):
+    path, file_name = SplitDirFile(s)
+    if path != None and not os.path.exists(path):
+        os.makedirs(path)
         return True
     else:
         return False
 
 
-def Log(level="DEBUG",is_back_up=True):
-    file_format="{time:YY-MM-DD HH:mm:ss} | <level>{level:<8}</level> | <level>{message}</level> "
-    cmd_format="<level>{level:<8}</level> | <level>{message}</level>"
-    err_id,out_id=(-1,-1)
+def MKOutput(path="."):
+    path = path + "/output/"
+    return MKDirsToFile(path)
+
+
+def Log(level="debug", is_full_print=False):
+    """
+    Initialize logger
+    """
+    level = level.upper()
+    cmd_format = "<level>{level:<8}</level> | <level>{message}</level>"
+    err_id, out_id = (-1, -1)
 
     logger.remove()
-    err_id=logger.add(sys.stderr,level=level,format=cmd_format,enqueue=True)
+    err_id = logger.add(sys.stderr, level=level, format=cmd_format, enqueue=True)
 
-    if is_back_up:
-        MKOutput()
-        out_id=logger.add("output/file.log",level="DEBUG",encoding="utf-8",enqueue=True,format=file_format,rotation="500 MB")
-    return err_id,out_id
+    _log_full_print = is_full_print
+
+    return err_id, out_id
+
+
+def AddLogFile(file_name="output/file.log", level="debug"):
+    level = level.upper()
+    if _log_full_print:
+        file_format = "{time:YY-MM-DD HH:mm:ss} | <level>{level:<8}</level> | <level>{message}</level> "
+    else:
+        file_format = "<level>{level:<8}</level> | <level>{message}</level> "
+    out_id = (-1, -1)
+    MKOutput()
+    out_id = logger.add(
+        file_name,
+        level=level,
+        encoding="utf-8",
+        enqueue=True,
+        format=file_format,
+        rotation="500 MB",
+        mode="w",
+    )
+    return out_id
 
 
 def Debug(s):
-    info=GetProcInfo()
-    logger.debug(info+s)
+    if _log_full_print:
+        s = GetProcInfo() + str(s)
+    else:
+        s = str(s)
+    logger.debug(s)
     sys.stdout.flush()
     sys.stderr.flush()
+
 
 def Info(s):
-    info=GetProcInfo()
-    logger.info(info+s)
+    if _log_full_print:
+        s = GetProcInfo() + str(s)
+    else:
+        s = str(s)
+    logger.info(s)
     sys.stdout.flush()
     sys.stderr.flush()
+
 
 def Success(s):
-    info=GetProcInfo()
-    logger.success(info+s)
+    if _log_full_print:
+        s = GetProcInfo() + str(s)
+    else:
+        s = str(s)
+    logger.success(s)
     sys.stdout.flush()
     sys.stderr.flush()
+
 
 def Warning(s):
-    info=GetProcInfo()
-    logger.warning(info+s)
+    if _log_full_print:
+        s = GetProcInfo() + str(s)
+    else:
+        s = str(s)
+    logger.warning(s)
     sys.stdout.flush()
     sys.stderr.flush()
 
+
 def Error(s):
-    info=GetProcInfo()
-    logger.error(info+s)
+    if _log_full_print:
+        s = GetProcInfo() + str(s)
+    else:
+        s = str(s)
+    logger.error(s)
     sys.stdout.flush()
     sys.stderr.flush()
+
+
+def Critical(s):
+    if _log_full_print:
+        s = GetProcInfo() + str(s)
+    else:
+        s = str(s)
+    logger.critical(s)
+    sys.stdout.flush()
+    sys.stderr.flush()
+
 
 def Complete():
     logger.complete()
 
 
-def GetProcInfo():    
-    pid=os.getpid()
-    p=psutil.Process(pid)
-    mem=psutil.virtual_memory()
-    mem_info=p.memory_full_info()
-    t=time.time()-p.create_time()
-    proc_info={"PID":pid,"PPID":p.ppid(),"Time":t,"USS":mem_info.uss/g_MB}
-    return " (P/PP:{}/{}, T(s):{:.1f}, U/F(MB):{}/{})\t| ".format(proc_info["PID"],proc_info["PPID"],proc_info["Time"],int(proc_info["USS"]),int(mem.free/g_MB))
+def GetProcInfo():
+    pid = os.getpid()
+    p = psutil.Process(pid)
+    mem = psutil.virtual_memory()
+    mem_info = p.memory_full_info()
+    t = time.time() - p.create_time()
+    proc_info = {"PID": pid, "PPID": p.ppid(), "Time": t, "USS": mem_info.uss / g_MB}
+    return " (P/PP:{}/{}, T(s):{:.1f}, U/F(MB):{}/{})\t| ".format(
+        proc_info["PID"],
+        proc_info["PPID"],
+        proc_info["Time"],
+        int(proc_info["USS"]),
+        int(mem.free / g_MB),
+    )
